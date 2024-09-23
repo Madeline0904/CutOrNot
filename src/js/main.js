f43377ad04bc75c009536bcf1dcf490583def076
MK <krawiecmag@gmail.com>
2024-09-23 18:46:10 +0200

    added review section

--- src/js/main.js
+++ src/js/main.js
@@ -63,4 +63,17 @@ const handleNavItemsAnimationIcon = () => {
 	})
 }
 
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+  });
+
+
 navBtn.addEventListener('click', handleNav)
