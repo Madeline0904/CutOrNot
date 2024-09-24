71c584b1aca7eaed6ebbe5e2ffe358f080ac2c82
MK <krawiecmag@gmail.com>
2024-09-24 13:10:59 +0200

    deleted swiper from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -68,24 +68,3 @@ const handleNavItemsAnimationIcon = () => {
 
 navBtn.addEventListener('click', handleNav)
 
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	effect: "cube",
-	autoplay: { delay: 5000 },
-	loop: true,
-  
-	grabCursor: true,
-	cubeEffect: {
-		shadow: true,
-		slideShadows: true,
-		shadowOffset: 20,
-		shadowScale: 0.94,
-	},
-  
-	// Navigation arrows
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  
-  });
