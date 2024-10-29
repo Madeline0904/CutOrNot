018b75bb74ed517390547032e3f4779cfae77373
MK <krawiecmag@gmail.com>
2024-10-29 10:39:52 +0100

    added angular

---
+++ src/app/reviews/reviews.component.ts
@@ -0,0 +1,28 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { Navigation, Pagination } from 'swiper/modules';
+
+register();
+@Component({
+  selector: 'app-reviews',
+  standalone: true,
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './reviews.component.html',
+  styleUrl: './reviews.component.scss',
+})
+export class ReviewsComponent implements OnInit {
+  ngOnInit() {
+    const swiperEl = document.querySelector('swiper-container');
+    if (!swiperEl) return;
+
+    const params = {
+      modules: [Navigation, Pagination],
+      injectStylesUrls: ['./assets/swiper.css'],
+    };
+
+    Object.assign(swiperEl, params);
+
+    swiperEl.initialize();
+  }
+}
